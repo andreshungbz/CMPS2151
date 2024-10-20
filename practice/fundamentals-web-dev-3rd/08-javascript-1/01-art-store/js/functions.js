20d959b3a13e2831c0f90dc10f0724a494c9901d
Andres Hung <andres.hung@outlook.com>
2024-10-19 22:11:30 -0600

    Complete art store exercise

--- practice/fundamentals-web-dev-3rd/08-javascript-1/01-art-store/js/functions.js
+++ practice/fundamentals-web-dev-3rd/08-javascript-1/01-art-store/js/functions.js
@@ -1,10 +1,71 @@
 /* define your functions here */
 
+const calculateTotal = (quantity, price) => {
+  return quantity * price;
+};
 
+const outputCartRow = (item, total) => {
+  document.write(`
+    <tr>
+        <td><img src="images/${item.product.filename}"></td>
+        <td>${item.product.title}</td>
+        <td>${item.quantity}</td>
+        <td>${item.product.price.toFixed(2)}</td>
+        <td>${total.toFixed(2)}</td>
+    </tr>
+  `);
+};
 
+const outputSubTotal = (cart) => {
+  const subtotal = cart.reduce((sum, item) => {
+    return sum + item.product.price * item.quantity;
+  }, 0);
 
+  document.write(`
+    <tr class="totals">
+        <td colspan="4">Subtotal</td>
+        <td>$${subtotal.toFixed(2)}</td>
+    </tr>
+  `);
 
+  return subtotal;
+};
 
+const outputTax = (subtotal, rate) => {
+  const tax = subtotal * rate;
 
+  document.write(`
+    <tr class="totals">
+        <td colspan="4">Tax</td>
+        <td>$${tax.toFixed(2)}</td>
+    </tr>
+  `);
 
-        
+  return tax;
+};
+
+const outputShipping = (subtotal, threshold) => {
+  const shipping = subtotal > threshold ? 0 : 40;
+
+  document.write(`
+    <tr class="totals">
+        <td colspan="4">Shipping</td>
+        <td>$${shipping.toFixed(2)}</td>
+    </tr>
+  `);
+
+  return shipping;
+};
+
+const outputGrandTotal = (subtotal, tax, shipping) => {
+  const grandTotal = subtotal + tax + shipping;
+
+  document.write(`
+    <tr class="totals">
+      <td colspan="4" class="focus">Grand Total</td>
+      <td class="focus">$${grandTotal}</td>
+    </tr>
+  `);
+
+  return grandTotal;
+};
