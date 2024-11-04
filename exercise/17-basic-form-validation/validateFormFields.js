d80be311096d9740441ac9e955ca904c582245a1
Andres Hung <andres.hung@outlook.com>
2024-11-04 15:25:50 -0600

    Complete index2.html

--- exercise/17-basic-form-validation/validateFormFields.js
+++ exercise/17-basic-form-validation/validateFormFields.js
@@ -1,4 +1,25 @@
-function alphanumeric(inputtext) {
-  const letters = /^[0-9a-zA-Z]+$/;
+const inputField = document.querySelector('#value1');
+const errorOutput = document.querySelector('#errorOutput');
+
+function alphanumeric(inputText) {
+  const regex = /^[0-9a-zA-Z]+$/;
+
+  if (regex.test(inputText)) {
+    alert('Your registration number has accepted: you can try another.');
+    inputField.focus();
+    return true;
+  } else {
+    alert('Please input alphanumeric characters only.');
+    return false;
+  }
 }
 
+const submitButton = document.querySelector('input[type="submit"]');
+submitButton.addEventListener('click', (e) => {
+  if (!alphanumeric(inputField.value)) {
+    e.preventDefault();
+    errorOutput.textContent = 'Please input alphanumeric characters only.';
+  } else {
+    errorOutput.textContent = '';
+  }
+});
