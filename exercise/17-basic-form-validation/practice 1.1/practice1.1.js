d96a5a1eaa9856cc7bdb64b50cea817a5b0276bf
Andres Hung <andres.hung@outlook.com>
2024-11-04 16:09:03 -0600

    Complete practice1.1.html

--- exercise/17-basic-form-validation/practice 1.1/practice1.1.js
+++ exercise/17-basic-form-validation/practice 1.1/practice1.1.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 /*    In Class Practice 3
       
 	  Course: CMPS2151 - Web Dev
@@ -9,3 +9,21 @@
 	  
 */
 
+const submitButton = document.querySelector('#submitButton');
+const passwordField = document.querySelector('#pwd');
+const passwordConfirmField = document.querySelector('#pwd2');
+const message = document.querySelector('#message');
+
+submitButton.addEventListener('click', (e) => {
+  const regex = new RegExp(passwordField.pattern);
+  if (!passwordField.value.match(regex)) {
+    e.preventDefault();
+    message.innerHTML =
+      'Your password must be at least 8 characters with at least one letter and one number';
+  } else if (passwordField.value != passwordConfirmField.value) {
+    e.preventDefault();
+    message.innerHTML = 'Your password confirmation does not match';
+  } else {
+    message.innerHTML = '';
+  }
+});
