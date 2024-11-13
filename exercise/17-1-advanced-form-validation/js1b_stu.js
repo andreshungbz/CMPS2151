6a3880f3f74bafefe99013cf24c977acc94aac9c
Andres Hung <andres.hung@outlook.com>
2024-11-12 23:04:17 -0600

    Complete exercise 17.1

--- exercise/17-1-advanced-form-validation/js1b_stu.js
+++ exercise/17-1-advanced-form-validation/js1b_stu.js
@@ -12,6 +12,16 @@
 
 // Validate the payment when the submit button is clicked Add eventListners here
 
+let subButton = document.getElementById('subButton');
+
+// Validate the payment when the submit button is clicked
+subButton.addEventListener('click', validateName);
+subButton.addEventListener('click', validateCard);
+subButton.addEventListener('click', validateNumber);
+subButton.addEventListener('click', validateMonth);
+subButton.addEventListener('click', validateYear);
+subButton.addEventListener('click', validateCVC);
+
 // validateName() Function: Check if the owner's name is entered on the card ---------------
 
 // Check if the owner's name is entered on the cardfunction
@@ -26,13 +36,74 @@ function validateName() {
 
 // validateCard() Function: Check if a credit card has been selected --------------------
 
+// Check if a credit card has been selected
+function validateCard() {
+  let card = document.forms.payment.elements.credit[0];
+  if (card.validity.valueMissing) {
+    card.setCustomValidity('Select your credit card');
+  } else {
+    card.setCustomValidity('');
+  }
+}
+
 // validateNumber() Function: Check if the card number is valid --------------------
+
+// Check if the card number is valid
+function validateNumber() {
+  let cNum = document.getElementById('cardNumber');
+  if (cNum.validity.valueMissing) {
+    cNum.setCustomValidity('Enter your card number');
+  } else if (cNum.validity.patternMismatch) {
+    cNum.setCustomValidity('Enter a valid card number');
+  } else if (luhn(cNum.value) === false) {
+    cNum.setCustomValidity('Enter a legitimate card number');
+  } else {
+    cNum.setCustomValidity('');
+  }
+}
 
 // validateMonth() Function: Check that a month is selected for the expiration date ------------------------
 
+// Check that a month is selected for the expiration date
+function validateMonth() {
+  let month = document.getElementById('expMonth');
+  if (month.selectedIndex === 0) {
+    month.setCustomValidity('Select the expiration month');
+  } else {
+    month.setCustomValidity('');
+  }
+}
+
 // validateYear() Function: Check that a year is selected for the expiration date ----------------------
 
+// Check that a year is selected for the expiration date
+function validateYear() {
+  let year = document.getElementById('expYear');
+  if (year.selectedIndex === 0) {
+    year.setCustomValidity('Select the expiration year');
+  } else {
+    year.setCustomValidity('');
+  }
+}
+
 // validateCVC() Function: Check that CVC is valid, check which card was selected and validate CVC ------------
+
+function validateCVC() {
+  // Determine which card was selected
+  let card = document.querySelector('input[name="credit"]:checked').value;
+  let cvc = document.getElementById('cvc');
+
+  // Validate the CVC value
+  if (cvc.validity.valueMissing) {
+    cvc.setCustomValidity('Enter your CVC number');
+  } else if (card === 'amex' && !/^\d{4}$/.test(cvc.value)) {
+    cvc.setCustomValidity('Enter a 4-digit number');
+  } else if (card !== 'amex' && !/^\d{3}$/.test(cvc.value)) {
+    cvc.setCustomValidity('Enter a 3-digit number');
+  } else {
+    cvc.setCustomValidity('');
+  }
+}
 
 /* ------- Luhn Algorithm used for Validating Credit Card Numbers   ----- */
 
